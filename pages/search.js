8582d325e689c99920e458165c8cf90744c9a50a
unknown <ogunsolatoluwalase@gmail.com>
2022-05-03 02:20:23 +0100

    intial commit

--- pages/search.js
+++ pages/search.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import Image from 'next/image'
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
-import { baseUrl,fetchApi } from '../../utils/fetchApi';
+import { baseUrl,fetchApi } from '../utils/fetchApi';
 import noresult from '../assets/images/noresult.svg'
 import SearchFilters from '../components/SearchFilters';
 import Property from "../components/Property";
