b511b9af32a6534ecb6781a51482b5acacb19670
Captain-awesome19 <ogunsolatoluwalase@gmail.com>
2025-10-20 12:01:44 +0800

    fixed

--- components/Property.jsx
+++ components/Property.jsx
@@ -6,38 +6,49 @@ import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import millify from 'millify';
-import DefaultImage from '../public/images/house.jpg'
 
-const DefaultImagee = '/images/house.jpg';
+// ❌ REMOVE this line
+// import DefaultImage from '../public/images/house.jpg';
 
-
-const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID  } }) => (
-    <Link href={`/property/${externalID}`} passHref>
-        <Flex flexWrap='wrap' w='420px' p='5' paddingTop='0px' justifyContent='flex-start' cursor='pointer' >
+const Property = ({
+  property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID },
+}) => (
+  <Link href={`/property/${externalID}`} passHref>
+    <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0px" justifyContent="flex-start" cursor="pointer">
       <Box>
-        <Image src={coverPhoto ? coverPhoto.url : DefaultImage} alt= "house" width={400} height={260} />
+        {/* ✅ Use the image path directly from the /public folder */}
+        <Image
+          src={coverPhoto?.url || '/images/house.jpg'}
+          alt="house"
+          width={400}
+          height={260}
+        />
       </Box>
-      <Box w='full'>
-        <Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
-          <Flex alignItems='center'>
-            <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
-            <Text fontWeight='bold' fontSize='lg'>AED {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
+
+      <Box w="full">
+        <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+          <Flex alignItems="center">
+            <Box paddingRight="3" color="green.400">
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text fontWeight="bold" fontSize="lg">
+              AED {millify(price)}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
           </Flex>
           <Box>
-            <Avatar size='sm' src={agency?. logo?. url} />
+            <Avatar size="sm" src={agency?.logo?.url} />
           </Box>
-      </Flex>
-      <Flex>
-      <Flex alignItems='center' p='1' justifyContent='space-between' w='250px' color='blue.400'>
-          {rooms}
-          <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
         </Flex>
-        <Text fontSize='lg'>
-          {title.length > 30 ? title.substring(0, 30) + '...' : title}
-        </Text>
-      </Flex>
+
+        <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="blue.400">
+          {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+        </Flex>
+
+        <Text fontSize="lg">{title.length > 30 ? title.substring(0, 30) + '...' : title}</Text>
       </Box>
-      </Flex>
-        </Link>
+    </Flex>
+  </Link>
 );
- export default Property;
+
+export default Property;
